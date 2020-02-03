62050367841ed4da94526180f0c826d3266cfaa2
dyago <dyagomatheus123@gmail.com>
2020-02-03 16:33:02 -0300

    update user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
 
-class ScheduleController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
